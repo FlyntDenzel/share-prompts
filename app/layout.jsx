01615cb9ed6ed99d96388ad831d6cbca05997df9
FlyntDenzel <flyntdenzel2004@gmail.com>
2024-03-25 12:31:29 +0100

    changed layout page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,28 @@
-import '@styles/globals.css';
-import Nav from '@components/Nav'
-import Provider from '@components/Provider'
-
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metaData = () => {
-    title: "Promptopia"
-    description: 'Discover and share prompts'
-}
+  title: "Promptopia";
+  description: "Discover and share prompts";
+};
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>
-            <div className='main'>
-                <div className='gradient'/>
-            </div>
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
-        </body>
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
